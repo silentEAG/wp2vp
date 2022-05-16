7399a6816c42c4da2392352885eb35f3ceb7f11f
silentEAG <1223610721@qq.com>
2022-05-16 20:28:58 +0800

    init

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,6 +4,34 @@ import pymysql
 import html2md
 import requests
 
+
+img_ls = ['https://tva4.sinaimg.cn/large/0072Vf1pgy1foxkiak921j31kw0w0nnl.jpg',
+          'https://tva4.sinaimg.cn/large/87c01ec7gy1frqe3zuxm4j21hc0u04qp.jpg',
+          'https://tva3.sinaimg.cn/large/0072Vf1pgy1foxlnx4mk0j31hc0u07k9.jpg',
+          'https://tva3.sinaimg.cn/large/0072Vf1pgy1foxlhey5i9j31kw0w0khl.jpg',
+          'https://tva1.sinaimg.cn/large/0072Vf1pgy1foxkfbb3c7j31kw0w04ou.jpg',
+          'https://tva1.sinaimg.cn/large/87c01ec7gy1frkjbhza1tj21hc0u0npd.jpg',
+          'https://tva1.sinaimg.cn/large/0072Vf1pgy1foxkizc9w8j31kw0w0e45.jpg',
+          'https://tva2.sinaimg.cn/large/0072Vf1pgy1foxlhcoxbbj31kw0w0tz3.jpg',
+          'https://tva3.sinaimg.cn/large/0072Vf1pgy1foxk3mu0ztj31hc0u07m5.jpg',
+          'https://tva2.sinaimg.cn/large/0072Vf1pgy1foxk6qy63sj31kw0w0twh.jpg',
+          'https://tva1.sinaimg.cn/large/0072Vf1pgy1foxkfylhhxj31hc0u04dv.jpg',
+          'https://tva1.sinaimg.cn/large/0072Vf1pgy1foxkfg4cj2j31hc0u01a9.jpg',
+          'https://tva1.sinaimg.cn/large/0072Vf1pgy1foxk7b1md7j31hc0u0qlb.jpg',
+          'https://tva2.sinaimg.cn/large/0072Vf1pgy1foxlojk0kbj31kw0w0tyb.jpg',
+          'https://tva2.sinaimg.cn/large/0072Vf1pgy1fodqnyvneij31hc0u01kx.jpg',
+          'https://tva4.sinaimg.cn/large/0072Vf1pgy1foxloigdl2j31kw0w0kib.jpg',
+          'https://tva4.sinaimg.cn/large/0072Vf1pgy1foxk782lbsj31hc0u0tr4.jpg',
+          'https://tva2.sinaimg.cn/large/0072Vf1pgy1foxlnk8bzcj31hc0u0qia.jpg',
+          'https://tva1.sinaimg.cn/large/0072Vf1pgy1foxkiijgosj31hc0u0140.jpg',
+          'https://tva3.sinaimg.cn/large/0072Vf1pgy1fodqo0e8fzj31hc0xcqv5.jpg',
+          'https://tva1.sinaimg.cn/large/0072Vf1pgy1foxk3wgs1qj31kw0w01jo.jpg',
+          'https://tva4.sinaimg.cn/large/0072Vf1pgy1foxkix1n7kj31kw0w0tzl.jpg',
+          'https://tva2.sinaimg.cn/large/0072Vf1pgy1foxkjdzgusj31hc0u0nfc.jpg',
+          'https://tva4.sinaimg.cn/large/0072Vf1pgy1foxkc2jnyej31hc0u04g2.jpg',
+          'https://tva4.sinaimg.cn/large/0072Vf1pgy1foxk3gtfinj31hc0u0k6t.jpg']
+
+
 term_dic = {}
 
 def get_data_in_db(sql):
